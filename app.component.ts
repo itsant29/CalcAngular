7bc73ff9df58e6f78260d85c19999d6741506827
Antoinette <antoinettehall29@gmail.com>
2021-04-07 23:46:28 +0800

    initial commit

---
+++ app.component.ts
@@ -0,0 +1,102 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'calcAngular';
+};
+constructor () { }
+
+ngOnInit(): void {
+
+}
+
+
+subDisplayText = '';
+mainDisplayText = '';
+operand1: number;
+operand2: number;
+operatorSet = false;
+calculationString = '';
+operator = '';
+answered = false;
+
+
+pressKey (key: string) {
+  if(key === '/' || key === 'x' || key === '-' || key === '+')
+  {
+    const lastKey = this.mainDisplayText[this.mainDisplayText.length -1];
+    if (lastKey === '/' || lastKey === 'x' || lastKey === '-' || lastKey === '+') 
+    {
+      this.operatorSet = true;
+    }
+    if ((this.operatorSet) || (this.mainDisplayText ==='')){
+      return;
+    }
+      this.operand1 = parseFloat(this.mainDisplayText);
+      this.operator = key;
+      this.operatorSet = true;
+    }
+    if (this.mainDisplayText.length === 10) {
+      return;
+    }
+    this.mainDisplayText += key;
+  }
+
+  allClear() {
+    this.mainDisplayText = '';
+    this.subDisplayText = '';
+    this.operatorSet = false;
+  }
+
+  cancelKey(){
+
+    if (this.mainDisplayText !="" ) {
+      this.mainDisplayText=this.mainDisplayText.substr(0, this.mainDisplayText.length-1)
+  }
+  // this.subDisplayText = '';
+  // this.operatorSet = true;
+
+}
+getAnswer() {
+  this.calculationString = this.mainDisplayText;
+  this.operand2 = parseFloat (this.mainDisplayText.split(this.operator)[1]);
+    if(this.operator === '/'){
+      this.subDisplayText = this.mainDisplayText;
+      this.mainDisplayText = (this.operand1 / this.operand2).toString();
+      this.subDisplayText = this.calculationString;
+      if (this.mainDisplayText.length >9) {
+        this.mainDisplayText = this.mainDisplayText.substr(0,0);
+      }
+    }
+    else if (this.operator === 'x'){
+      this.subDisplayText = this.mainDisplayText;
+      this.mainDisplayText = (this.operand1 * this.operand2).toString();
+      this.subDisplayText = this.calculationString;
+      if (this.mainDisplayText.length > 9) {
+        this.mainDisplayText = 'ERROR';
+        this.subDisplayText = 'Range Exceeded';
+      }
+    }
+    else if (this.operator === '-') {
+      this.subDisplayText = this.mainDisplayText;
+      this.mainDisplayText = (this.operand1 - this.operand2).toString();
+      this.subDisplayText = this.calculationString;
+    }
+    else if (this.operator === '+'){
+      this.subDisplayText = this.mainDisplayText;
+      this.mainDisplayText = (this.operand1 + this.operand2).toString();
+      this.subDisplayText = this.calculationString;
+      if (this.mainDisplayText.length > 9) {
+        this.mainDisplayText = 'ERROR';
+        this.subDisplayText = 'Range Exceeded';
+      }
+    } else {
+      this.subDisplayText = 'ERROR: Invalid Operation';
+    }
+    this.answered = true;
+}
+
